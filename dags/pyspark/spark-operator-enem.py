95d1311586ddd39c42269024aeb181da2cf8645c
Mateus Lira <mateusc.lira@gmail.com>
2022-06-19 17:41:41 -0300

    operator update

--- dags/pyspark/spark-operator-enem.py
+++ dags/pyspark/spark-operator-enem.py
@@ -1,17 +1,11 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 
-# set conf
-conf = (
-SparkConf()
-    .set("spark.hadoop.fs.s3a.fast.upload", True)
-    .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-    .set('spark.hadoop.fs.s3a.aws.credentials.provider', 'com.amazonaws.auth.EnvironmentVariableCredentialsProvider')
-    .set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.3')
-)
 
+aws_access_key_id = Variable.get('aws_access_key_id')
+aws_secret_access_key = Variable.get('aws_secret_access_key')
 # apply config
-sc = SparkContext(conf=conf).getOrCreate()
+sc = SparkContext().getOrCreate()
     
 
 if __name__ == "__main__":
