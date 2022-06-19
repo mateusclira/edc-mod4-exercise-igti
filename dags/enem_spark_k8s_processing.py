a3e0bf4365847817a360c12310457c643b109ed6
Mateus Lira <mateusc.lira@gmail.com>
2022-06-19 19:44:56 -0300

    spark

--- dags/enem_spark_k8s_processing.py
+++ dags/enem_spark_k8s_processing.py
@@ -49,7 +49,7 @@ with DAG(
     converte_parquet_monitor = SparkKubernetesSensor(
         task_id='converte_parquet_monitor',
         namespace="airflow",
-        application_name="{{ task_instance.xcom_pull(task_ids='converte_parquet')['metadata']['name'] }}",
+        application_name="{{ task_instance.xcom_pull(task_id='converte_parquet')['metadata']['name'] }}",
         kubernetes_conn_id="kubernetes_default",
     )
 
