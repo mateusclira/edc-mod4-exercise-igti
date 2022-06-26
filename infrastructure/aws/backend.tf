f3a8d369b26241fd109c9e592e86bec4172edc0a
Mateus Lira <mateusc.lira@gmail.com>
2022-06-26 15:06:42 -0300

    backend changes

--- infrastructure/aws/backend.tf
+++
@@ -1,8 +0,0 @@
-# Backend configuration require a AWS storage bucket.
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-igti-mateus"
-    key    = "state/igti/edc/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- infrastructure/aws/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.region_id
-}
-
-data "aws_caller_identity" "mateusclira" {}
-data "aws_region" "us-east-1" {}
-
-
-
